0a1a12ab544f1dedf8bfa2c75a0e42a0730df236
Daniel Oludare <oludee24@gmail.com>
2025-11-10 10:54:09 +0100

    Update Index.js

--- api/Index.js
+++ api/Index.js
@@ -11,7 +11,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: ["https://lagosrenthelp.ng/"], // only allow your site
+    origin: ["https://lagosrenthelp.ng"], // only allow your site
     credentials: true, // if you’re using cookies or tokens
   })
 );
