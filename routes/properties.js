d9611aa3360d24a01357937c53cbf361bb067349
Adewonise John <adewonisekayode@gmail.com>
2025-11-15 23:04:07 +0100

    this commit adds update property

--- routes/properties.js
+++ routes/properties.js
@@ -5,6 +5,7 @@ const {
   getPropertyById,
   createProperty,
   deactivateProperty,
+  updateProperty
 } = require("../controllers/propertyController");
 const upload = require("../middleware/upload");
 const checkSubscription = require("../middleware/checkSubscription");
@@ -16,5 +17,12 @@ router.get("/:id", getPropertyById);
 router.post("/properties", auth, upload.array("images", 10), createProperty);
 
 router.put("/:id/deactivate", auth, deactivateProperty);
+
+router.put(
+  "/update/:id",
+  auth, 
+  upload.array("images", 10),
+  updateProperty
+);
 
 module.exports = router;
