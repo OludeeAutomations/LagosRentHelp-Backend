facd804a6d77e589c6e819d52a2a3320e9bb8952
sun3350 <isaacayomide2359@gmail.com>
2026-04-14 06:31:45 +0100

    setting update

--- api/server.js
+++ api/server.js
@@ -47,8 +47,8 @@ console.log(
   "CORS middleware applied. Allowed origin:",
   typeof corsOptions.origin === "boolean" ? "ALL (debug)" : "specific list",
 );
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "15mb" }));
+app.use(express.urlencoded({ extended: true, limit: "15mb" }));
 app.use(cookieParser());
 
 app.use("/api/auth", require("../routes/auth"));
